85183ecec6dca0474ab95dd228f156ccaf30dc90
solitdio079 <solitdio079@gmail.com>
2025-01-20 16:14:03 +0300

    saved

---
+++ index.mjs
@@ -0,0 +1,46 @@
+import express from 'express'
+import { } from 'dotenv/config'
+import sendgrid from '@sendgrid/mail'
+import cors from 'cors'
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
+const corsOptions = {
+  origin: [
+    'http://localhost:5173',
+    'https://www.romacoffeetea.com',
+    'https://romacoffeetea.com',
+  ],
+
+  //credentials: true,
+  optionsSuccessStatus: 200,
+  credentials: true,
+}
+const app = express()
+
+const port = process.env.PORT
+app.use(cors(corsOptions))
+app.use(express.json())
+
+app.post("/franchise", async (req, res) => {
+    const { fullName, email, phone, town, district, message } = req.body
+     var msg = {
+       to: 'solitdio079@gmail.com',
+       from: process.env.EMAIL,
+       subject: 'Franchising Talebi',
+       text: `Ad Soyad:${fullName}.\r\n\ Email:${email}.\r\n\ Telefon:${phone}.\r\n\ il:${town}.\r\n\ ilçe:${district}\r\n\ Mesaj: ${message}`,
+       html: `<ul>
+         <li>Ad Soyad:${fullName}</li>
+         <li>Email:${email}</li>
+         <li>Telefon:${phone}</li>
+         <li>il:${town}</li>
+         <li>ilçe:${district}</li>
+         <li>Mesaj: ${message}</li>
+       </ul>`,
+     }
+    sendgrid.send(msg)
+     return  res.send({msg: 'Göderildi'})
+})
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+})
